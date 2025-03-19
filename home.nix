7f62dd5cb7a7b34cdd631e87441b4c8edf293069
Aceroph <aos92@proton.me>
2025-03-19 15:02:53 -0400

    Added git credentials

--- home.nix
+++ home.nix
@@ -2,6 +2,12 @@
 
 {
 	imports = [ ./modules/neovim.nix ];
+
+	programs.git = {
+		enable = true;
+		userName = "Aceroph";
+		userEmail = "aos92@proton.me";
+	};
 	
 	home.stateVersion = "24.05";
 }
