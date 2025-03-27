3de0fb7eb133f046d69f55023c4d3cd23b9a7740
Aceroph <aos92@proton.me>
2025-03-27 13:14:24 -0400

    Now using a monospaced font for the bar

--- modules/DE/i3/default.nix
+++ modules/DE/i3/default.nix
@@ -61,7 +61,7 @@
             position = "bottom";
             statusCommand = "${pkgs.i3status-rust}/bin/i3status-rs ~/.config/i3status-rust/config-bottom.toml";
             fonts = {
-              names = [ "FiraCode Nerd Font" ];
+              names = [ "FiraCode Nerd Font Mono" ];
               style = "Regular";
               size = 12.0;
             };
