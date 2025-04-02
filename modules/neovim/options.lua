0313367961e28b4ca7490e002f1ed1c2c01d0a39
Aceroph <aos92@proton.me>
2025-04-02 08:05:58 -0400

    Added live grep keybind

--- modules/neovim/options.lua
+++ modules/neovim/options.lua
@@ -6,4 +6,5 @@ vim.o.shiftwidth = 4
 -- Keybinds
 vim.keymap.set("n", "<leader>e", "<cmd>lua vim.diagnostic.open_float()<CR>", { desc = "Show error at cursor" })
 vim.keymap.set("n", "<leader>ff", require("telescope.builtin").find_files, { desc = "Telescope - Find files" })
+vim.keymap.set("n", "<leader>fg", require("telescope.builtin").live_grep, { desc = "Telescope - Live grep" })
 vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], { desc = "ToggleTerm - Exit terminal" })
