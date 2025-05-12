3834b5cb2c6db191a7c6bb6c26599cf0f72e6321
Aceroph <aos92@proton.me>
2025-05-12 19:08:26 -0400

    Added a greeter and the sway init command because i forgot to

--- modules/greetd/default.nix
+++ modules/greetd/default.nix
@@ -1,3 +1,10 @@
+{ pkgs }:
+
 {
-  services.greetd.enable = true;
+  services.greetd = {
+    enable = true;
+    settings.default_session = {
+      command = "${pkgs.greetd.greetd}/bin/agreety --cmd sway";
+    };
+  };
 }
