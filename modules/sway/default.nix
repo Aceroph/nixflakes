baeafb84c19dc72a7b43278d9142150703c48ee2
Aceroph <aos92@proton.me>
2025-05-12 19:39:30 -0400

    Waybar config and more sway

--- modules/sway/default.nix
+++ modules/sway/default.nix
@@ -1,7 +1,6 @@
 {
   username,
   pkgs,
-  config,
   lib,
   ...
 }:
@@ -12,19 +11,28 @@
       enable = true;
       package = pkgs.swayfx;
       checkConfig = false;
-      config = {
-        defaultWorkspace = "workspace number 1";
-        bars = [
-          { command = "waybar"; }
-        ];
-        keybinds =
-          let
-            modifier = config.wayland.windowManager.sway.config.modifier;
-          in
-          lib.mkOptionDefault {
+      config =
+        let
+          modifier = "Mod4";
+          terminal = "alacritty";
+        in
+        {
+          defaultWorkspace = "workspace number 1";
+          bars = [
+            {
+              command = "waybar";
+              fonts = {
+                names = [ "GohuFont 14 Nerd Font Mono" ];
+                style = "Regular";
+                size = 14.0;
+              };
+            }
+          ];
+          keybindings = lib.mkOptionDefault {
             "${modifier}+q" = "kill";
           };
-      };
+          inherit modifier terminal;
+        };
       extraConfig = "corner_radius 15";
     };
   };
