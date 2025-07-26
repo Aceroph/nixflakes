c680e7984e675abc595a5d186b5d414bbb4edca8
Aceroph <aos92@proton.me>
2025-07-26 19:34:47 -0400

    foot: Edited colors to include darker versions

--- colors.nix
+++ colors.nix
@@ -3,24 +3,22 @@
 let
   colorscheme = {
     primary = "ffff66";
-    primary-muted = "666629";
+    primary-muted = "99993d";
     secondary = "66b3ff";
+    secondary-muted = "3d6b99";
     tertiary = "b366ff";
+    tertiary-muted = "6b3d99";
     danger = "ff6666";
+    danger-muted = "993d3d";
     warning = "ffb366";
+    warning-muted = "996b3d";
     success = "b3ff66";
+    success-muted = "6b993d";
     background = "262626";
     background-lighter = "333333";
     background-darker = "191919";
     text = "f8f8d3";
-    text-muted = "787866";
-    primary0 = "ffff1a";
-    primary1 = "ffff33";
-    primary2 = "ffff4d";
-    primary3 = "ffff66";
-    primary4 = "ffff80";
-    primary5 = "ffff99";
-    primary6 = "ffffb3";
+    text-muted = "91917c";
   };
 in
 {
--- config/foot/default.nix
+++ config/foot/default.nix
@@ -16,13 +16,14 @@
         regular5 = colors.tertiary;
         regular6 = colors.warning;
         regular7 = colors.text;
-        bright0 = colors.primary0;
-        bright1 = colors.primary1;
-        bright2 = colors.primary2;
-        bright3 = colors.primary3;
-        bright4 = colors.primary4;
-        bright5 = colors.primary5;
-        bright6 = colors.primary6;
+        bright0 = colors.background-muted;
+        bright1 = colors.danger-muted;
+        bright2 = colors.success-muted;
+        bright3 = colors.primary-muted;
+        bright4 = colors.secondary-muted;
+        bright5 = colors.tertiary-muted;
+        bright6 = colors.warning-muted;
+        bright7 = colors.text-muted;
       };
       main = {
         font = "D2CodingLigature Nerd Font:size=12";
