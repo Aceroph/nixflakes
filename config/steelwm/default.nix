0602abf5baa6bec9d461eb21339330600d1dc525
Aceroph <aos92@proton.me>
2025-08-06 15:37:41 -0400

    steelwm: Swap xterm for ghostty

--- config/steelwm/default.nix
+++ config/steelwm/default.nix
@@ -6,7 +6,7 @@
     libinput.enable = true;
     display = 0;
     displayManager.startx.enable = true;
-    desktopManager.xterm.enable = true;
+    desktopManager.xterm.enable = false;
     windowManager.dwm = {
       enable = true;
       package = steelwm.packages.x86_64-linux.default;
