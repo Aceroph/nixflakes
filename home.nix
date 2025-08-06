0a37514d9353d875cf1fbbeb3af343db12747f2c
Aceroph <aos92@proton.me>
2025-08-06 16:01:13 -0400

    zsh: Set variables to make man pages colored

--- home.nix
+++ home.nix
@@ -127,16 +127,18 @@
       setopt prompt_subst
       PROMPT='%F{yellow}%~%f ''${vcs_info_msg_0_}'
 
-      export LESS_TERMCAP_mb=$'\e[1;32m'
-      export LESS_TERMCAP_md=$'\e[1;32m'
-      export LESS_TERMCAP_me=$'\e[0m'
-      export LESS_TERMCAP_se=$'\e[0m'
-      export LESS_TERMCAP_so=$'\e[01;33m'
-      export LESS_TERMCAP_ue=$'\e[0m'
-      export LESS_TERMCAP_us=$'\e[1;4;31m'
 
       eval "$(zoxide init zsh)"
     '';
+    sessionVariables = {
+      LESS_TERMCAP_mb = "\e[1;32m";
+      LESS_TERMCAP_md = "\e[1;32m";
+      LESS_TERMCAP_me = "\e[0m";
+      LESS_TERMCAP_se = "\e[0m";
+      LESS_TERMCAP_so = "\e[01;33m";
+      LESS_TERMCAP_ue = "\e[0m";
+      LESS_TERMCAP_us = "\e[1;4;31m";
+    };
     shellAliases = {
       cd = "z";
       fetch = "fastfetch";
