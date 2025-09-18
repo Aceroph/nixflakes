f47469ad48acbd593f21e56ad0918bea1ff1b70d
Aceroph <aos92@proton.me>
2025-09-18 11:04:53 -0400

    nvim: Remove dot folders from showing up in telescope

--- config/neovim/plugins/telescope.lua
+++ config/neovim/plugins/telescope.lua
@@ -6,7 +6,7 @@ require("telescope").setup {
     },
     defaults = {
         file_ignore_patterns = {
-            '^\\.git/',
+            '%..*/',
             'node_modules/',
         },
     },
