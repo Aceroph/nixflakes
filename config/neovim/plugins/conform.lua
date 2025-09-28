ba25b074a233633e4eaff1fcb93b1d742faa5784
Aceroph <aos92@proton.me>
2025-09-27 20:31:34 -0400

    neovim: Add global for vim, remove unused plugin config file and reorder linters

--- config/neovim/plugins/conform.lua
+++ config/neovim/plugins/conform.lua
@@ -4,14 +4,15 @@ require("conform").setup {
         lsp_format = "fallback",
     },
     formatters_by_ft = {
+        c = { "clang-format" },
+        css = { "prettierd" },
+        html = { "prettierd" },
+        javascript = { "prettierd" },
+        json = { "jsonfmt" },
+        lua = { "stylua" },
         nix = { "nixfmt" },
         python = { "isort", "black" },
         rust = { "rustfmt" },
-        javascript = { "prettierd" },
         typescript = { "prettierd" },
-        html = { "prettierd" },
-        css = { "prettierd" },
-        json = { "jsonfmt" },
-        c = { "clang-format" }
     },
 }
