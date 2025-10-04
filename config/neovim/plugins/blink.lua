e47ed9139be83d9b04e615025440ea0b73a52a59
Aceroph <aos92@proton.me>
2025-10-04 13:30:57 -0400

    neovim: Fix deprecated use of require('lspconfig')

--- config/neovim/plugins/blink.lua
+++ config/neovim/plugins/blink.lua
@@ -1,37 +1,36 @@
 local servers = {
-    angularls = {},
-    rust_analyzer = {},
-    pyright = {},
-    lua_ls = {},
-    cssls = {},
-    html = {},
-    jsonls = {},
-    volar = {},
-    nil_ls = {},
-    ts_ls = {},
-    nimls = {},
-    clangd = {},
+	angularls = {},
+	rust_analyzer = {},
+	pyright = {},
+	lua_ls = {},
+	cssls = {},
+	html = {},
+	jsonls = {},
+	volar = {},
+	nil_ls = {},
+	ts_ls = {},
+	nimls = {},
+	clangd = {},
 }
 
-local lspconfig = require("lspconfig")
 local blink = require("blink.cmp")
 for server, config in pairs(servers) do
-    config.capabilities = blink.get_lsp_capabilities(config.capabilities)
-    lspconfig[server].setup(config)
+	config.capabilities = blink.get_lsp_capabilities(config.capabilities)
+	vim.lsp.config[server].setup(config)
 end
 
-blink.setup {
-    cmdline = { enabled = false },
-    completion = {
-        documentation = {
-            auto_show = true,
-            auto_show_delay_ms = 150,
-        },
-        ghost_text = { enabled = true },
-        keyword = { range = "full" },
-        list = { selection = { preselect = false, auto_insert = true } },
-    },
-    keymap = { preset = "enter" },
-    signature = { enabled = true },
-    sources = { default = { 'lsp', 'path', 'snippets' } },
-}
+blink.setup({
+	cmdline = { enabled = false },
+	completion = {
+		documentation = {
+			auto_show = true,
+			auto_show_delay_ms = 150,
+		},
+		ghost_text = { enabled = true },
+		keyword = { range = "full" },
+		list = { selection = { preselect = false, auto_insert = true } },
+	},
+	keymap = { preset = "enter" },
+	signature = { enabled = true },
+	sources = { default = { "lsp", "path", "snippets" } },
+})
