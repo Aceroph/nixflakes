912e00448eab0871d5d39e1adb3eec10c93251ca
Aceroph <aos92@proton.me>
2025-10-29 13:02:03 -0400

    vicinae: Create custom theme

--- config/vicinae/default.nix
+++ config/vicinae/default.nix
@@ -1,13 +1,34 @@
+{ colors, ... }:
+
 {
   services.vicinae = {
     enable = true;
     autoStart = true;
+    themes.custom-dark = {
+      version = 1;
+      name = "Custom theme";
+      description = "A custom theme generated from colors.nix";
+      variant = "dark";
+      colors.core = {
+        background = colors.prefixed.background;
+        foreground = colors.prefixed.foreground;
+        red = colors.prefixed.red;
+        yellow = colors.prefixed.yellow;
+        green = colors.prefixed.green;
+        cyan = colors.prefixed.aqua;
+        blue = colors.prefixed.blue;
+        purple = colors.prefixed.purple;
+      };
+    };
     settings = {
       faviconService = "twenty";
-      font.size = 11;
+      font = {
+        size = 11;
+        family = "Sono";
+      };
       popToRootOnClose = false;
       rootSearch.searchFiles = false;
-      theme.name = "vicinae-dark";
+      theme.name = "custom-dark";
       window = {
         csd = false;
         opacity = 0.8;
