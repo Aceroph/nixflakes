1c0059f92e460f1b9632c54bc1094bb5982223cb
Aceroph <aos92@proton.me>
2025-11-04 10:04:47 -0500

    feat: Add phinger cursors

--- home.nix
+++ home.nix
@@ -113,6 +113,16 @@
     xwayland-satellite
   ];
 
+  ###########
+  # Pointer #
+  ###########
+  home.pointerCursor = {
+    name = "phinger-cursors-light";
+    package = pkgs.phinger-cursors;
+    size = 32;
+    gtk.enable = true;
+  };
+
   ############
   # Services #
   ############
